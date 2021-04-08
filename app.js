60c8c55846204e667bd22448da226e45eca67280
mateomh <mmojicah.84@gmail.com>
2021-04-08 11:34:51 -0500

    Add create and edit functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = angular.module('expensesApp',['ngRoute']);
 
 // Services => are a way to have shared information between controllers
 // Services are like global variables for your application but have to
-// be included in the controller. $scope is an angular service
+// be included in the controller. $scope is an Angular service
 app.factory('Expenses', () => {
   const service = {}
 
@@ -12,6 +12,15 @@ app.factory('Expenses', () => {
     {id: 3,description: 'tickets',amount: 100},
     {id: 4,description: 'gas',amount: 40}
   ];
+
+  service.saveNew = (item) => {
+    service.items.push(item);
+  }
+
+  // service.updateItem = (item) => {
+  //   // service.items[item.id] = item;
+  //   // console.log(item);
+  // }
 
   return service;
 })
@@ -28,7 +37,7 @@ app.config(['$routeProvider', ($routeProvider) => {
   .when('/expenses/new',{
     templateUrl: './views/form.html'
   })
-  .when('/expense/edit/:id',{
+  .when('/expenses/edit/:id',{
     templateUrl: './views/form.html'
   })
   .otherwise({
@@ -52,6 +61,23 @@ app.controller('ExpensesViewController',['$scope', 'Expenses', ($scope, Expenses
   }
 }]);
 
-app.controller('CreateEditExpenseController',['$scope', '$routeParams', ($scope, $routeParams) => {
-  $scope.message = 'This is the message from the addres bar ID: ' + $routeParams.id;
-}]);
+app.controller(
+  'CreateEditExpenseController',
+  ['$scope', '$routeParams', 'Expenses', '$location', 
+  ($scope, $routeParams, Expenses, $location) =>
+  {
+    $scope.expense = { id: 7, description: 'movies', amount: 30};
+    
+    if($routeParams.id != undefined)
+    {
+      $scope.expense = Expenses.items[$routeParams.id - 1];
+    }
+    
+    $scope.save = () => {
+      if($routeParams.id == undefined) {
+        Expenses.saveNew($scope.expense);
+      }
+      $location.path('/');
+    }
+  }
+]);
